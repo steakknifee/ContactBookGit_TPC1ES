bffe979901be2108f3173cbd715a73ca3cc3f6c4
pedro <pms.almeida@campus.fct.unl.pt>
2026-09-18 20:01:58 +0100

    Implemented the functions getNumber() and existPhones() in the Main class, which will be called by the GN and EP commands respectively. Added the constants and logic required for the GN and EP commands to the Main class.

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,9 @@ public class Main {
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
     public static final String QUIT           = "Q";
+    public static final String GET_NUMBER     = "GN";
+    public static final String EXIST_PHONES   = "EP";
+
 
     //Constantes que definem as mensagens para o utilizador
     public static final String CONTACT_EXISTS = "contactBook.Contact already exists.";
@@ -24,6 +27,9 @@ public class Main {
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
+    public static final String PHONE_NOT_EXIST = "Phone number does not exist.";
+    public static final String PHONES_EXIST = "There are contacts that share phone numbers.";
+    private static final String PHONES_NOT_EXIST = "All contacts have different phone numbers.";
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -52,6 +58,12 @@ public class Main {
                     break;
                 case LIST_CONTACTS:
                     listAllContacts(cBook);
+                    break;
+                case GET_NUMBER:
+                    getNumber(in, cBook);
+                    break;
+                case EXIST_PHONES:
+                    existPhones(cBook);
                     break;
                 default:
                     System.out.println(COMMAND_ERROR);
@@ -146,5 +158,21 @@ public class Main {
             }
         }
         else System.out.println(BOOK_EMPTY);
+    }
+
+    private static void getNumber(Scanner in, ContactBook cBook){
+        int phone = in.nextInt(); in.nextLine();
+        if (cBook.hasPhoneNumber(phone)) {
+            String name = cBook.searchByPhoneNumber(phone);
+            System.out.println(name);
+        }
+        else System.out.println(PHONE_NOT_EXIST);
+    }
+
+    private static void existPhones(ContactBook contactBook){
+        if (contactBook.searchSamePhones()) {
+            System.out.println(PHONES_EXIST);
+        }
+        else System.out.println(PHONES_NOT_EXIST);
     }
 }
